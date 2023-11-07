c333dd640d91744ac01b506dc0b93cfe65b7eb39
Alex Barbero <barrufat@hotmail.com>
2023-11-07 13:11:56 +0100

    Add AppStyled

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import FilmsPage from "../../pages/FilmsPage/FilmsPage";
+import AppStyled from "./AppStyled";
 
 const App = () => {
   return (
-    <div className="main-container">
+    <AppStyled>
       <h1>Movies App</h1>
       <Routes>
         <Route path="/" element={<Navigate to="/films" />} />
         <Route path="/films" element={<FilmsPage />} />
       </Routes>
-    </div>
+    </AppStyled>
   );
 };
 
