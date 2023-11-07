a2ee6c6b70d58bcb40ccc284319000614aaccf56
Alex Barbero <barrufat@hotmail.com>
2023-11-07 20:17:47 +0100

    Add FilmCard styles

---
+++ src/components/FilmCard.tsx/FilmCardStyled.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+const FilmsListStyled = styled.article`
+  width: 300px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  border-radius: 10px 50px 10px 50px;
+  position: relative;
+
+  .film {
+    &__image {
+      width: 300px;
+      height: 200px;
+      border-radius: 10px 50px 10px 50px;
+    }
+
+    &__info {
+      width: 100%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      justify-content: center;
+      height: 40%;
+      background-color: #ffdace8e;
+      border-radius: 0 0 10px 50px;
+      position: absolute;
+      padding: 20px;
+      bottom: 0;
+    }
+
+    &__title {
+      margin: 0;
+    }
+  }
+`;
+
+export default FilmsListStyled;
--- src/components/FilmsList/FilmsListStyled.ts
+++ src/components/FilmsList/FilmsListStyled.ts
@@ -7,39 +7,6 @@ const FilmsListStyled = styled.section`
   gap: 20px;
   margin-top: 20px;
 
-  .film {
-    width: 300px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    border-radius: 10px 50px 10px 50px;
-    position: relative;
-
-    &__image {
-      width: 300px;
-      height: 200px;
-      border-radius: 10px 50px 10px 50px;
-    }
-
-    &__info {
-      width: 100%;
-      display: flex;
-      flex-direction: column;
-      align-items: flex-end;
-      justify-content: center;
-      height: 40%;
-      background-color: #ffdace8e;
-      border-radius: 0 0 10px 50px;
-      position: absolute;
-      padding: 20px;
-      bottom: 0;
-    }
-
-    &__title {
-      margin: 0;
-    }
-  }
-
   @media (max-width: 400px) {
     grid-template-columns: repeat(1, 1fr);
   }
