601ddb14d2b3f2aedee5278c1702638245537b13
Alex Barbero <barrufat@hotmail.com>
2023-11-08 23:05:32 +0100

    Fix FilmCard test

--- src/components/FilmCard.tsx/FilmCard.test.tsx
+++ src/components/FilmCard.tsx/FilmCard.test.tsx
@@ -2,13 +2,19 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import FilmCard from "./FilmCard";
 import filmsMock from "../../mocks/filmsMock";
+import { Provider } from "react-redux";
+import { store } from "../../store";
 
 describe("Given a FilmCard component", () => {
   describe("When it recieves the Avatar film", () => {
     test("It should show 'Avatar' in a heading", () => {
       const expectedFilmTitle = "Avatar";
 
-      render(<FilmCard film={filmsMock[0]} />);
+      render(
+        <Provider store={store}>
+          <FilmCard film={filmsMock[0]} />
+        </Provider>,
+      );
 
       const filmTitle = screen.getByRole("heading", {
         name: expectedFilmTitle,
