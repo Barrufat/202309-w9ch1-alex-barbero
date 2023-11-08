4341e6c4b8d15c49573ba7d4b2491a5124e29a4d
Alex Barbero <barrufat@hotmail.com>
2023-11-08 23:02:34 +0100

    Dispatch toggleWatchedFilms action

--- src/components/FilmCard.tsx/FilmCard.tsx
+++ src/components/FilmCard.tsx/FilmCard.tsx
@@ -1,23 +1,42 @@
+import { useDispatch } from "react-redux";
 import { FilmStructure } from "../../store/feature/films/types";
+import Button from "../Button/Button";
 import FilmCardStyled from "./FilmCardStyled";
+import { toggleWatchedFilmsActionCreator } from "../../store/feature/films/filmsSlice";
 
 interface FilmCardProps {
   film: FilmStructure;
 }
 
 const FilmCard = ({ film }: FilmCardProps) => {
+  const dispatch = useDispatch();
+
+  const toggleCardIsWatched = (id: number): void => {
+    dispatch(toggleWatchedFilmsActionCreator(id));
+  };
+
   return (
     <FilmCardStyled>
       <img
-        className="film__image"
+        className={`film__image ${film.isWatched ? "film__image-watched" : ""}`}
         src={film.Images[0]}
         alt={film.Title}
         width="300"
         height="199.175"
       />
       <div className="film__info">
-        <h3 className="film__title">{film.Title}</h3>
-        <span className="film__year">{film.Year}</span>
+        <Button
+          type={"button"}
+          title="isWatched-button"
+          actionOnClick={() => {
+            toggleCardIsWatched(film.id);
+          }}
+          text={`${film.isWatched ? "watched" : "unwatched"}`}
+        />
+        <div className="film__data-box">
+          <h3 className="film__title">{film.Title}</h3>
+          <span className="film__year">{film.Year}</span>
+        </div>
       </div>
     </FilmCardStyled>
   );
